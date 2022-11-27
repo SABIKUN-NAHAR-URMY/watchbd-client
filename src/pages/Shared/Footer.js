37c289c2c99636678288d765bc2b0a0ac3823775
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-11-27 20:27:35 +0600

    footer section done

--- src/pages/Shared/Footer.js
+++ src/pages/Shared/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import logo from '../../images/logo.png';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+        <footer className="footer rounded-lg justify-around items-center mt-14 p-4 bg-slate-200 text-black">
+            <div className="items-center grid-flow-col">
+                <img className='w-32' src={logo} alt="" />
+                <p>Copyright © 2022 - All right reserved</p>
+            </div>
+            <div className="footer p-10 ">
+                <div>
+                    <span className="footer-title">ABOUT WatchBD</span>
+                    <p>There are several different avenues you can use to sell <br /> your watch, such as selling or consigning directly to dealers, <br /> consigning with auction houses or posting to online classifieds or forums. <br /> Not every one will work for every watch.</p>
+                </div>
+            </div>
+        </footer>
     );
 };
 
